5dfb304dc4665b55c427eeb0629b1375b71bb95a
Jamshed Rabadi <jamshedrabadi@gmail.com>
2026-03-28 16:46:44 -0700

    server schema additions

--- src/db/schema/server.schema.js
+++ src/db/schema/server.schema.js
@@ -1,7 +1,11 @@
 import {
     pgTable,
+    uniqueIndex,
     serial,
     varchar,
+    integer,
+    boolean,
+    timestamp,
 } from "drizzle-orm/pg-core";
 
 export const server = pgTable("server", {
@@ -12,4 +16,21 @@ export const server = pgTable("server", {
     ip: varchar("ip", { length: 15 }).notNull(),
 
     port: varchar("port", { length: 5 }).notNull(),
-});
+
+    country: varchar("country", { length: 255 }).notNull(),
+
+    current_map: varchar("current_map", { length: 100 }).notNull(),
+
+    current_players: integer("current_players").default(0).notNull(),
+
+    max_players: integer("max_players").default(0).notNull(),
+
+    is_active: boolean("is_active").default(true).notNull(),
+
+    last_seen: timestamp("last_seen_at", { withTimezone: true }),
+
+    created_at: timestamp("created_at", { withTimezone: true }).defaultNow().notNull(),
+}, (table) => [
+    uniqueIndex("server_ip_port_unique")
+        .on(table.ip, table.port),
+]);
