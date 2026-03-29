290fb3090b45c6a7e5a5ff1717984970cb56898f
Jamshed Rabadi <jamshedrabadi@gmail.com>
2026-03-28 20:23:22 -0700

    fixed get all servers api

--- src/mappers/server.mapper.js
+++ src/mappers/server.mapper.js
@@ -1,3 +1,8 @@
+import {
+    COUNTRY_FLAG_URL,
+    COUNTRY_FLAG_EXTENSION,
+} from "../constants/country.constants.js";
+
 export const mapGetServerListResponse = (serverData) => {
     const mappedPlayerData = serverData.map((server) => {
         return {
@@ -5,10 +10,14 @@ export const mapGetServerListResponse = (serverData) => {
             serverName: server.server_name,
             serverIp: server.server_ip,
             serverPort: server.server_port,
-            serverCountry: server.server_country,
+            serverCountryId: server.server_country_id,
+            serverCountryName: server.server_country_name,
+            serverCountryCode: server.server_country_code,
+            serverCountryFlag: `${COUNTRY_FLAG_URL}${server.server_country_code}${COUNTRY_FLAG_EXTENSION}`,
             serverMaxPlayers: server.server_max_players,
             serverCurrentPlayers: server.server_current_players,
-            serverCurrentMap: server.server_current_map,
+            serverMapId: server.server_map_id,
+            serverMapName: server.server_map_name,
             serverLastSeen: server.server_last_seen,
             serverDisplayServer: server.server_display_server,
             serverDisplayOrder: server.server_display_ordeer,
--- src/services/server.service.js
+++ src/services/server.service.js
@@ -1,7 +1,9 @@
-import { asc } from "drizzle-orm";
+import { asc, eq } from "drizzle-orm";
 
 import { db } from "../db/db-connection.js";
 import { servers } from "../db/schema/servers.schema.js";
+import { country } from "../db/schema/country.schema.js";
+import { maps } from "../db/schema/maps.schema.js";
 
 export const getServerListFromDb = async () => {
     try {
@@ -11,15 +13,24 @@ export const getServerListFromDb = async () => {
                 server_name: servers.name,
                 server_ip: servers.ip,
                 server_port: servers.port,
-                server_country: servers.country_id,
+                server_country_id: servers.country_id,
+                server_country_name: country.name,
+                server_country_code: country.code,
                 server_max_players: servers.max_players,
                 server_current_players: servers.current_players,
-                server_current_map: servers.map_id,
+                server_map_id: servers.map_id,
+                server_map_name: maps.name,
                 server_last_seen: servers.last_seen,
                 server_display_server: servers.display_server,
                 server_display_order: servers.display_order,
             })
             .from(servers)
+            .innerJoin(country,
+                eq(country.id, servers.country_id),
+            )
+            .innerJoin(maps,
+                eq(maps.id, servers.map_id),
+            )
             .orderBy(
                 asc(servers.display_order),
                 asc(servers.id),
