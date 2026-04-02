46b59de145e68351c093b6e2f4e22e5b97e197ad
Jamshed Rabadi <jamshedrabadi@gmail.com>
2026-04-02 01:52:01 -0700

    minor linting changes

--- src/utils/common.utils.js
+++ src/utils/common.utils.js
@@ -1,12 +1,14 @@
 export const convertToNumber = (value) => {
     value ||= 0;
     const num = Number(value);
+
     return !Number.isNaN(num) ? num : 0;
 };
 
 export const convertToDecimalNumber = (value, decimals = 2) => {
     value ||= 0;
     const num = Number(value);
+
     return !Number.isNaN(num) ? Number(num.toFixed(decimals)) : 0;
 };
 
